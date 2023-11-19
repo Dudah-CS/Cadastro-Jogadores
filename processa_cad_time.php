02f45cb08545f8dbfecda5b00a702e520a139001
Eduarda <135717344+Dudah-CS@users.noreply.github.com>
2023-11-19 19:57:50 -0300

    Create processa_cad_time.php

---
+++ processa_cad_time.php
@@ -0,0 +1,25 @@
+<?php
+//O processa_cad será bastante usado para diferenciarmos os arquivos de CADASTROS para os que vão levar e trazer informações
+
+	include_once("conn.php");
+	$pos = $_POST['txtTime'];
+	$result_pos = "INSERT INTO tbtime(nomeTime) VALUES ('$pos')";
+
+	$resultado_pos = mysqli_query($conn, $result_pos);
+	
+	if(mysqli_affected_rows($conn) != 0){
+				echo "
+					<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=cadTime.html'>
+					<script type=\"text/javascript\">
+						alert(\"Time cadastrada com Sucesso.\");
+					</script>
+				";	
+			}else{
+				echo "
+					<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=cadTime.html'>
+					<script type=\"text/javascript\">
+						alert(\"Erro ao cadastrar!\");
+					</script>
+				";	
+			}
+?>
